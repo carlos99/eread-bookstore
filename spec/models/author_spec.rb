5023e5653647e92f249a7d72abd323f1723ff674
Carlos Orellana <carlos@dccolorweb.com>
2016-07-01 01:05:21 -0600

    Finishig the first basic specs for the validation of first_name and last_name for the authors

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -4,16 +4,19 @@ RSpec.describe Author, :type => :model do
   it "requires the first name" do
     author = Fabricate.build(:author, first_name: nil)
     expect(author).not_to be_valid
-    expect(author.errors[:first_name].any?).to be_true
+    expect(author.errors[:first_name].any?).to be_truthy
   end
 
   it "requires the last name" do
-
+    author = Fabricate.build(:author, last_name: nil)
+    expect(author).not_to be_valid
+    expect(author.errors[:last_name].any?).to be_truthy
   end
 
   describe "#full_name" do
     it "returns the full name of the author" do
-
+      author = Fabricate(:author, first_name: "Carlos", last_name: "Orellana")
+      expect(author.full_name).to eq("Carlos Orellana")
     end
   end
 end
