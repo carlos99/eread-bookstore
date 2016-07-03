9796b8b737681a2d473ed860416e22f33341eb04
Carlos Orellana <carlos@dccolorweb.com>
2016-07-02 19:31:58 -0600

    Author success path create spec is done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,8 +7,21 @@ class AuthorsController < ApplicationController
     @author = Author.new
   end
 
+  def create
+    @author = Author.new(author_params)
+    @author.save
+    flash[:success] = "Author has been created"
+    redirect_to @author
+  end
+
   def show
     #require 'pry';binding.pry
     @author = Author.find(params[:id])
   end
+
+  private
+
+    def author_params
+      params.require(:author).permit(:first_name, :last_name)
+    end
 end
--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -25,4 +25,26 @@ RSpec.describe AuthorsController, :type => :controller do
     end
   end
 
+  describe "POST #create" do
+    context "a successful create" do
+      it "save the new author object" do
+        post :create, author: Fabricate.attributes_for(:author)
+        expect(Author.count).to eq(1)
+      end
+
+      it "redirects to the author show action" do
+        post :create, author: Fabricate.attributes_for(:author)
+        expect(response).to redirect_to author_path(Author.last) #or Author.first. author_path requires the id of the author
+      end
+
+      it "an unsuccessful create" do
+        post :create, author: Fabricate.attributes_for(:author)
+        expect(flash[:success]).to eq("Author has been created")
+      end
+    end
+
+    context "an unsuccessful create" do
+
+    end
+  end
 end
