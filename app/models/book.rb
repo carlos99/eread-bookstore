886b24f74c5aacb332c7667d2d3e24b19dbcc7e4
Carlos Orellana <carlos@dccolorweb.com>
2016-07-04 00:19:25 -0600

    Adding the necesary validations to the book model. Now all book specs are passing

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,8 @@
 class Book < ActiveRecord::Base
   belongs_to :publisher
+  has_many :publications, dependent: :destroy
+
+  validates :title, :isbn, :description, :published_at, :publisher_id,   presence: true
+  validates :page_count, numericality: { only_integer: true, greater_than: 0 }
+  validates :price, numericality: {greater_than_or_equal_to: 0.0 }
 end
