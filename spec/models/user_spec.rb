c82c0177d1819915f22766166b42c10ddd1447bd
Carlos Orellana <carlos@dccolorweb.com>
2016-07-10 16:32:49 -0600

    Adding production condition to carrierwave.rb file in order to avoid conflicts with specs

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.describe User, :type => :model do
+  it "require a first name" do
+
+  end
+
+  it "require a last name" do
+
+  end
+
+  it "require a email address" do
+
+  end
+
+  it "require password" do
+
+  end
+
+  describe "#full_name" do
+    it "requires full name" do
+
+    end
+  end
+end
