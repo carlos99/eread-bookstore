68897fe9daf4a24e2f912ea0d2ff160b60cc740a
Carlos Orellana <carlos.cw2106@gmail.com>
2016-07-26 22:43:02 -0600

    Implementing the basic part of the books catalogs

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,9 @@ Rails.application.routes.draw do
   get '/sign_up', to: 'users#new', as: 'sign_up'
   get '/sign_in', to: "sessions#new", as: "sign_in"
   post '/sign_in', to: "sessions#create"
+
   resource :session
+
+  resources :catalogs, only: [:index, :show]
 
 end
