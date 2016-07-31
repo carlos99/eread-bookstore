0054f58a9474e145b517491df750fcda70390ee7
Carlos Orellana <carlos.cw2106@gmail.com>
2016-07-30 21:39:03 -0600

    Implemting the create action of card_items. Creating the add_boo_to_item model method, this method will look if we have a book or we need to create one.

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -2,7 +2,9 @@ class CartItemsController < ApplicationController
   before_action :set_cart
 
   def create
-    #code
+    @cart_item = @cart.add_book_to_items(params[:book_id])
+    if @cart_item
+      redirect_to @cart_item.cart
+    end
   end
-
 end
