9bee88d1dd5373856516f65889fca6d162569ef3
Carlos Orellana <carlos.cw2106@gmail.com>
2016-07-31 16:58:36 -0600

    Creating the order and order_item models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160731210533) do
+ActiveRecord::Schema.define(version: 20160731225604) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,26 @@ ActiveRecord::Schema.define(version: 20160731210533) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "order_items", force: :cascade do |t|
+    t.integer  "order_id"
+    t.integer  "book_id"
+    t.decimal  "price",      precision: 8, scale: 2
+    t.integer  "quantity"
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
+  end
+
+  add_index "order_items", ["book_id"], name: "index_order_items_on_book_id", using: :btree
+  add_index "order_items", ["order_id"], name: "index_order_items_on_order_id", using: :btree
+
+  create_table "orders", force: :cascade do |t|
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "orders", ["user_id"], name: "index_orders_on_user_id", using: :btree
+
   create_table "publications", force: :cascade do |t|
     t.integer  "author_id"
     t.datetime "created_at", null: false
@@ -99,6 +119,9 @@ ActiveRecord::Schema.define(version: 20160731210533) do
   add_foreign_key "books", "publishers"
   add_foreign_key "cart_items", "books"
   add_foreign_key "cart_items", "carts"
+  add_foreign_key "order_items", "books"
+  add_foreign_key "order_items", "orders"
+  add_foreign_key "orders", "users"
   add_foreign_key "publications", "authors"
   add_foreign_key "publications", "books"
 end
