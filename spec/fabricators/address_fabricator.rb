374ed3cca716b551ef4939df33cff15737590ff5
Carlos Orellana <carlos.cw2106@gmail.com>
2016-07-31 15:38:43 -0600

    Adding the address fabricator. Adding the addres validations and the address complety spec

--- spec/fabricators/address_fabricator.rb
+++ spec/fabricators/address_fabricator.rb
@@ -1,7 +1,6 @@
 Fabricator(:address) do
-  address_line1 "MyString"
-  address_line2 "MyString"
-  city          "MyString"
-  zipcode       "MyString"
-  user          nil
+  address_line1 { Faker::Address.street_address }
+  address_line2 { Faker::Address.street_address }
+  city { Faker::Address.city }
+  zipcode { Faker::Address.zip_code }
 end
