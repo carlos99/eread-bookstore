220e809d5cd92acc9562ae62888c420720838804
Carlos Orellana <carlos.cw2106@gmail.com>
2016-07-31 12:37:01 -0600

    creating the _cart and create.js files in order to work using ajax. Now the use can add and delete without refreshing the page

--- app/controllers/catalogs_controller.rb
+++ app/controllers/catalogs_controller.rb
@@ -1,4 +1,5 @@
 class CatalogsController < ApplicationController
+  before_action :set_cart_if_session
 
   def index
     @books = Book.all
@@ -6,6 +7,12 @@ class CatalogsController < ApplicationController
 
   def show
     @book = Book.find params[:id]
+  end
+
+  private
+
+  def set_cart_if_session
+    set_cart if session[:cart_id]
   end
 
 end
