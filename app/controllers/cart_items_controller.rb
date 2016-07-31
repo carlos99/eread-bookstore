e99802ff4d2ce5ceac31ee39d6f5ae4ab1d68b99
Carlos Orellana <carlos.cw2106@gmail.com>
2016-07-30 23:45:13 -0600

    Adding the total line and total sale specs into cart and cart_items. Adding the corresponding methods to cart and cart_item model in order to calculate the total of line and total of sale

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -5,6 +5,8 @@ class CartItemsController < ApplicationController
     @cart_item = @cart.add_book_to_items(params[:book_id])
     if @cart_item
       redirect_to @cart_item.cart
+    else
+      redirect_to catalogs_path
     end
   end
 end
