af5111021f8ed3b0a83f606a1ebc6641f3a43128
Carlos Orellana <carlos.cw2106@gmail.com>
2016-07-31 21:56:43 -0600

    Finishing the configuration of the email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,8 +15,6 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = {:host => "localhost:3000"}
-  config.action_mailer.delivery_method = :smtp
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -37,6 +35,19 @@ Rails.application.configure do
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
+
+  config.action_mailer.default_url_options = {:host => "localhost:3000"}
+  config.action_mailer.delivery_method = :smtp
+
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['SENGRID_USERNAME'],
+    :password => ENV['SENGRID_PASSWORD'],
+    :domain => 'example.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
